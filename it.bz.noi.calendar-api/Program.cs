87f11f0d5149c386acc1ad9357a89a9f98b463d1
Tobias Burger <tobias_burger@hotmail.com>
2022-08-31 11:29:30 +0200

    Implementation of the API with OData capabilities

--- it.bz.noi.calendar-api/Program.cs
+++ it.bz.noi.calendar-api/Program.cs
@@ -5,23 +5,28 @@ using Microsoft.Graph;
 using WebApplication = Microsoft.AspNetCore.Builder.WebApplication;
 using Microsoft.Graph.Extensions;
 using Microsoft.Extensions.Options;
-
-dotenv.net.DotEnv.Load();
-
-static string GetEnv(string key)
-{
-    return Environment.GetEnvironmentVariable(key) ?? throw new Exception($"Environment variable {key} not set.");
-}
-
-static string? TryGetEnv(string key)
-{
-    return Environment.GetEnvironmentVariable(key);
-}
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton(serviceProvider =>
 {
+    dotenv.net.DotEnv.Load();
+
+    static string GetEnv(string key)
+    {
+        return Environment.GetEnvironmentVariable(key) ?? throw new Exception($"Environment variable {key} not set.");
+    }
+
+    static string? TryGetEnv(string key)
+    {
+        return Environment.GetEnvironmentVariable(key);
+    }
+
     return new Settings(
         Username: GetEnv("USERNAME"),
         Password: GetEnv("PASSWORD"),
@@ -33,12 +38,14 @@ builder.Services.AddSingleton(serviceProvider =>
     );
 });
 
+builder.Services.AddControllers();
+builder.Services.ConfigureOptions<CalendarApiODataOptions>();
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer();
-
 builder.Services.ConfigureOptions<ConfigureJwtBearerOptions>();
 
 builder.Services.AddAuthorization(options =>
@@ -63,61 +70,33 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
-async Task<IEnumerable<Event>> GetEventsFromCalendar(
-    GraphServiceClient client,
-    Settings settings,
-    string meetingRoom,
-    DateOnly start,
-    DateOnly end
-)
-{
-    // Only query for the next `NUMBER_OF_EVENTS` number of events
-    var result = await client.Users[meetingRoom]
-        .CalendarView
-        .Request(new[] {
-            new QueryOption("startDateTime", start.ToString("yyyy-MM-dd")),
-            new QueryOption("endDateTime", end.ToString("yyyy-MM-dd"))
-        })
-        .OrderBy("start/dateTime")
-        .Top(settings.NumberOfEvents)
-        .GetAsync();
-    return result
-        .Select(@event =>
-            new Event(
-                MeetingRoom: meetingRoom,
-                Subject: @event.Subject,
-                Body: @event.Body.Content,
-                StartDateTime: @event.Start.ToDateTimeOffset(),
-                EndDateTime: @event.End.ToDateTimeOffset()
-            )
-        );
-}
-
-async Task<IEnumerable<Event>> GetAllCalendarEvents(GraphServiceClient client, Settings settings)
-{
-    // Get events for the next sixty days.
-    var start = DateOnly.FromDateTime(DateTime.Today);
-    var end = start.AddDays(60);
-    var eventss = await Task.WhenAll(
-        settings.MeetingRooms.Select(meetingRoom =>
-            GetEventsFromCalendar(client, settings, meetingRoom, start, end)
-        )
-    );
-    var events =
-        eventss
-            .SelectMany(events => events)
-            .OrderBy(e => e.StartDateTime)
-            .Take(settings.NumberOfEvents);
-    return events;
-}
-
-app.MapGet("/", GetAllCalendarEvents).RequireAuthorization("noi-auth");
+app.MapControllers();
 
 app.Run();
 
-record Event(string MeetingRoom, string Subject, string Body, DateTimeOffset StartDateTime, DateTimeOffset EndDateTime);
+public record Event(string Id, string MeetingRoom, string Subject, string Body, DateTimeOffset StartDateTime, DateTimeOffset EndDateTime);
 
-record Settings(string Username, string Password, string TenantId, string ClientId, string OpenIdAuthority, string[] MeetingRooms, int NumberOfEvents);
+public record Settings(string Username, string Password, string TenantId, string ClientId, string OpenIdAuthority, string[] MeetingRooms, int NumberOfEvents);
+
+class CalendarApiODataOptions : IConfigureNamedOptions<ODataOptions>
+{
+    private readonly Settings _settings;
+
+    public CalendarApiODataOptions(Settings settings)
+    {
+        _settings = settings;
+    }
+
+    public void Configure(string name, ODataOptions options)
+    {
+        Configure(options);
+    }
+
+    public void Configure(ODataOptions options)
+    {
+        options.Select().Filter().SetMaxTop(_settings.NumberOfEvents).OrderBy().Count();
+    }
+}
 
 class ConfigureJwtBearerOptions : IConfigureNamedOptions<JwtBearerOptions>
 {
@@ -141,5 +120,74 @@ class ConfigureJwtBearerOptions : IConfigureNamedOptions<JwtBearerOptions>
             NameClaimType = "preferred_username",
             ValidateAudience = false
         };
+    }
+}
+
+public class CalendarController : ODataController
+{
+    private readonly GraphServiceClient _client;
+    private readonly Settings _settings;
+
+    public CalendarController(GraphServiceClient client, Settings settings)
+    {
+        _client = client;
+        _settings = settings;
+    }
+
+    //[Authorize("noi-auth")]
+    [EnableQuery]
+    [HttpGet("/")]
+    public async Task<List<Event>> Get()
+    {
+        return (await GetAllCalendarEvents(_client, _settings)).ToList();
+    }
+
+    async Task<IEnumerable<Event>> GetAllCalendarEvents(GraphServiceClient client, Settings settings)
+    {
+        // Get events for the next sixty days.
+        var start = DateOnly.FromDateTime(DateTime.Today);
+        var end = start.AddDays(60);
+        var eventss = await Task.WhenAll(
+            settings.MeetingRooms.Select(meetingRoom =>
+                GetEventsFromCalendar(client, settings, meetingRoom, start, end)
+            )
+        );
+        var events =
+            eventss
+                .SelectMany(events => events)
+                .OrderBy(e => e.StartDateTime)
+                .Take(settings.NumberOfEvents);
+        return events;
+    }
+
+    async Task<IEnumerable<Event>> GetEventsFromCalendar(
+        GraphServiceClient client,
+        Settings settings,
+        string meetingRoom,
+        DateOnly start,
+        DateOnly end
+    )
+    {
+        // Only query for the next `NUMBER_OF_EVENTS` number of events
+        var result = await client.Users[meetingRoom]
+            .CalendarView
+            .Request(new[] {
+                new QueryOption("startDateTime", start.ToString("yyyy-MM-dd")),
+                new QueryOption("endDateTime", end.ToString("yyyy-MM-dd"))
+            })
+            .OrderBy("start/dateTime")
+            .Top(settings.NumberOfEvents)
+            .GetAsync();
+        return result
+            .Select(@event =>
+                new Event(
+                    Id: @event.Id,
+                    MeetingRoom: meetingRoom,
+                    Subject: @event.Subject,
+                    Body: @event.Body.Content,
+                    StartDateTime: @event.Start.ToDateTimeOffset(),
+                    EndDateTime: @event.End.ToDateTimeOffset()
+                )
+            );
     }
 }
